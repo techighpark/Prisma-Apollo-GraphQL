84c253e103fc51ce3036772ca41dd710512443c0
techigh <techigh.b@gmail.com>
2022-01-16 20:23:15 +0900

    COMPLETE

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import schema from './schema';
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
   schema,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-server.listen().then(() => {
-  console.log(`🚀 Server ready at http://localhost:4000`);
+server.listen(PORT).then(() => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}✅`);
 });
